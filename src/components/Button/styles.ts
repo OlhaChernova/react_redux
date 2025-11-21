3161371bce5e83a72b02aea8ab7e68300a7ad37d
OlhaChernova1987 <chernova.olha1987@gmail.com>
2025-11-21 17:04:56 +0100

    add lesson17

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,22 @@
+import styled from "@emotion/styled";
+
+interface ButtonStyledProps {
+  isDanger?: boolean;
+}
+
+export const ButtonComponent = styled.button<ButtonStyledProps>`
+  width: 100px;
+  padding: 20px;
+  background: ${({ isDanger }) => (isDanger ? "#761d0bff" : "#1f27f5")};
+  font-size: 20px;
+  color: white;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+
+  &:disabled {
+  background: grey;
+}
+`;
+
+
---
+++ src/routes/routes.tsx
@@ -0,0 +1,9 @@
+import type { RoutePage } from "./types";
+import Home from "pages/Home/Home";
+import Lesson17 from "lessons/Lesson17/Lesson17";
+
+export const routesData: RoutePage[] = [
+  { path: "/", element: <Home /> },
+  { path: "/lesson17", element: <Lesson17 /> },
+  { path: "*", element: "Page not found" },
+];
